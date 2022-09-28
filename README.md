3ab390e1454f1365790266136418ff3d638265db
Andrew <andrewsa1006@gmail.com>
2022-09-28 10:15:08 -0600

    Tweaked homepage to look better, as well as added an actual README file.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# Welcome to Partable.com!
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was created as a proof-of-concept for a friend so he could present some innovative ideas to his company. This is not intended to be a fully functional or fully flushed out application at this time.
 
 ## Available Scripts
 
@@ -14,57 +14,8 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
+![Application Flow chart](./src//assets/Capture.PNG?raw=true "Application Flow")
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+The application uses AWS Amplify, which should handle auto scaling automatically. Since we don’t need login functionality and its a simple nearly static site, nothing more complex needed for configuration.
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Requests made from the site will be sent to API gateway (this includes initial page load to fetch items in database). Images for parts will be stored in S3 with a link to the bucket stored in RDS (the database). From API gateway, requests will then be forwarded to AWS Lambda to run various serverless functions (creating new items, updating inventory). A separate function will handle generating and sending the email out to an external email client (like support@partable.com) using SES.
--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import "./Body.css";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
 
 const Body = () => {
@@ -46,36 +47,80 @@ const Body = () => {
           <Button>Request Service Quote</Button>
         </Tab>
       </Tabs>
-      <div className="mt-3 extra-content bg-secondary text-white">
-        <h3 className="mb-3">Insert customer testimonial</h3>
-        <p>
-          Here is a long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here
-          is a long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a
-          long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a long
-          customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a long
-          customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good.
-        </p>
-      </div>
-      <div className="mt-3 extra-content bg-light">
-        <h3 className="mb-3">Insert customer testimonial</h3>
-        <p>
-          Here is a long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here
-          is a long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a
-          long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a long
-          customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a long
-          customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good.
-        </p>
-      </div>
-      <div className="mt-3 extra-content bg-secondary text-white">
-        <h3 className="mb-3">Insert customer testimonial</h3>
-        <p>
-          Here is a long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here
-          is a long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a
-          long customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a long
-          customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good. Here is a long
-          customer testimonial about why partable is so good. Here is a long customer testimonial about why partable is so good.
-        </p>
-      </div>
+      <Card className="mt-4">
+        <Card.Title style={{ fontSize: "2.2rem" }} className="mt-2">
+          Don't just take our word for it - Listen to our customers!
+        </Card.Title>
+        <div style={{ border: "2px solid black", borderRadius: "10px" }} className="mt-3 extra-content bg-secondary text-white">
+          <h3 style={{ textAlign: "start", marginLeft: "10px", borderBottom: "1px black solid" }} className="mb-3  mt-2">
+            Mark, From X -
+          </h3>
+          <div style={{ display: "flex" }}>
+            <img
+              style={{ margin: "10px", border: "1px solid white", borderRadius: "5px" }}
+              width="210"
+              height="210"
+              src={require("../assets/profile1.jpg")}
+              alt="profile 1"
+            />
+            <p style={{ padding: "10px" }}>
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis qui voluptate dolorem at, quas, sequi beatae est numquam corrupti
+              accusantium fugit consectetur delectus voluptatum dolore! Natus numquam error aliquid. Exercitationem temporibus harum esse sed. Sunt
+              itaque rerum veniam enim quos, explicabo temporibus, accusamus earum quae ut laudantium esse dignissimos ipsam quasi non. Aut velit
+              aspernatur magnam alias a soluta modi, illum necessitatibus, ipsa ex ducimus impedit quam fuga iste pariatur non, reiciendis hic ipsam
+              commodi quisquam sed unde minima? Inventore et sint commodi nesciunt quo magnam, placeat vitae facilis corporis, pariatur dolorem
+              repellendus delectus suscipit! Veniam, dolore, harum unde hic molestiae doloremque repellendus est sed ipsam dicta aliquid iure ab.
+              Omnis fuga quidem doloremque et illo obcaecati atque sit possimus."
+            </p>
+          </div>
+        </div>
+        <div style={{ border: "2px solid black", borderRadius: "10px" }} className="mt-3 extra-content bg-light">
+          <h3 style={{ textAlign: "end", marginRight: "10px", borderBottom: "1px black solid" }} className="mb-3  mt-2">
+            - Lisa, from somwhere
+          </h3>
+          <div style={{ display: "flex" }}>
+            <p style={{ padding: "10px" }}>
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis qui voluptate dolorem at, quas, sequi beatae est numquam corrupti
+              accusantium fugit consectetur delectus voluptatum dolore! Natus numquam error aliquid. Exercitationem temporibus harum esse sed. Sunt
+              itaque rerum veniam enim quos, explicabo temporibus, accusamus earum quae ut laudantium esse dignissimos ipsam quasi non. Aut velit
+              aspernatur magnam alias a soluta modi, illum necessitatibus, ipsa ex ducimus impedit quam fuga iste pariatur non, reiciendis hic ipsam
+              commodi quisquam sed unde minima? Inventore et sint commodi nesciunt quo magnam, placeat vitae facilis corporis, pariatur dolorem
+              repellendus delectus suscipit! Veniam, dolore, harum unde hic molestiae doloremque repellendus est sed ipsam dicta aliquid iure ab.
+              Omnis fuga quidem doloremque et illo obcaecati atque sit possimus."
+            </p>
+            <img
+              style={{ margin: "10px", border: "1px solid white", borderRadius: "5px" }}
+              width="210"
+              height="210"
+              src={require("../assets/profile2.jpg")}
+              alt="profile 2"
+            />
+          </div>
+        </div>
+        <div style={{ border: "2px solid black", borderRadius: "10px" }} className="mt-3 extra-content bg-secondary text-white">
+          <h3 style={{ textAlign: "start", marginLeft: "10px", borderBottom: "1px black solid" }} className="mb-3  mt-2">
+            Matthew, (He's a nice guy) -
+          </h3>
+          <div style={{ display: "flex" }}>
+            <img
+              style={{ margin: "10px", border: "1px solid white", borderRadius: "5px" }}
+              width="210"
+              height="210"
+              src={require("../assets/profile3.jpg")}
+              alt="profile 3"
+            />
+            <p style={{ padding: "10px" }}>
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis qui voluptate dolorem at, quas, sequi beatae est numquam corrupti
+              accusantium fugit consectetur delectus voluptatum dolore! Natus numquam error aliquid. Exercitationem temporibus harum esse sed. Sunt
+              itaque rerum veniam enim quos, explicabo temporibus, accusamus earum quae ut laudantium esse dignissimos ipsam quasi non. Aut velit
+              aspernatur magnam alias a soluta modi, illum necessitatibus, ipsa ex ducimus impedit quam fuga iste pariatur non, reiciendis hic ipsam
+              commodi quisquam sed unde minima? Inventore et sint commodi nesciunt quo magnam, placeat vitae facilis corporis, pariatur dolorem
+              repellendus delectus suscipit! Veniam, dolore, harum unde hic molestiae doloremque repellendus est sed ipsam dicta aliquid iure ab.
+              Omnis fuga quidem doloremque et illo obcaecati atque sit possimus."
+            </p>
+          </div>
+        </div>
+      </Card>
     </div>
   );
 };
